9efd498635299e8cd2df398e68b9127dcc992d14
hemanth-bonala-11 <bonalah.it.21@nitj.ac.in>
2023-11-26 18:22:00 +0530

    setup completed

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 require('./config/DbConfig.js').dbconnect()
+app.use(fileUpload())
 
+const cloudinary_connect = require('./config/CloudinaryConfig.js')
+cloudinary_connect.cloudinaryConnect();
 const PORT = process.env.PUBLIC_PORT || 4000;
+
+const routes = require('./routes/FileUploadRoutes.js')
+app.use(routes)
 
 app.get('/', (req, res) => {
     res.send('Hello World');
